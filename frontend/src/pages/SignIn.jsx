2256bc220520a2d7693b6a9f790264d99b7b4a55
Piploy163 <pornpimon.srt@g.swu.ac.th>
2024-12-02 02:32:39 +0700

    final

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import OAuth from '../components/OAuth';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   signInStart,
@@ -67,7 +66,6 @@ export default function SignIn() {
         >
           {loading ? 'Loading...' : 'Sign In'}
         </button>
-        <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Dont have an account?</p>
